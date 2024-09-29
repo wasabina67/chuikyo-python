dd1d5eb5b60f052e7af14ad0c147732c55fc6f99
wasabina67 <wasabina67@gmail.com>
2024-09-29 12:57:29 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,22 @@
 import requests  # type: ignore
+import xlsxwriter  # type: ignore
 from bs4 import BeautifulSoup  # type: ignore
+
+
+def output_xlsx(headers, rows):
+    ths = headers.findAll("th")
+    th_list = [th.text for th in ths]
+
+    row_list = []
+    for row in rows:
+        row_list.append([td.text.lstrip("\n") for td in row.findAll("td")])
+
+    wb = xlsxwriter.Workbook("docs/output.xlsx")
+    ws = wb.add_worksheet()
+    ws.write_row(0, 0, th_list)
+    for i, row in enumerate(row_list):
+        ws.write_row(i + 1, 0, row)
+    wb.close()
 
 
 def validate_headers(headers):
@@ -39,6 +56,8 @@ def main():
     td_num, td_event_date, td_agenda, _, td_docs, _ = tds  # noqa
     latest_num = td_num.text
     print(latest_num)
+
+    output_xlsx(headers, rows[1:])
 
 
 if __name__ == "__main__":
