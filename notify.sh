a0c889716ae824e3a0c05fea4acae8de48d44107
wasabina67 <wasabina67@gmail.com>
2024-09-29 13:08:06 +0900

    Update notify.sh

--- notify.sh
+++ notify.sh
@@ -3,3 +3,8 @@
 NUM=`cat num.txt`
 MESSAGE="${NUM} の資料が公開されました。https://www.mhlw.go.jp/stf/shingi/shingi-chuo_128154.html"
 URL="https://notify-api.line.me/api/notify"
+
+curl -X POST \
+    -H "Authorization: Bearer ${ACCESS_TOKEN}" \
+    -F "message=${MESSAGE}" \
+    ${URL}
