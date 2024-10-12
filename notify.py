0ce09e4fdd50a1729612cde3ebf7a63b529dedfb
wasabina67 <wasabina67@gmail.com>
2024-10-12 14:10:04 +0900

    Update notify.py

--- notify.py
+++ notify.py
@@ -1,5 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+from linebot.v3 import messaging  # type: ignore
+
+load_dotenv(override=True)
+
+
 def main():
-    pass
+    access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+    configuration = messaging.Configuration(access_token=access_token)  # noqa
 
 
 if __name__ == "__main__":
